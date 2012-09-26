cb685d6d805876a093abebb1f36d852b1378698e
quek <quekshuy@gmail.com>
2012-09-26 13:20:33 +0800

    Product can now be associated to multiple sites

--- cartridge/shop/admin.py
+++ cartridge/shop/admin.py
@@ -115,7 +115,7 @@ class ProductImageAdmin(TabularDynamicInlineAdmin):
 
 product_fieldsets = deepcopy(DisplayableAdmin.fieldsets)
 product_fieldsets[0][1]["fields"][1] = ("status", "available")
-product_fieldsets[0][1]["fields"].extend(["content", "categories"])
+product_fieldsets[0][1]["fields"].extend(["content", "categories", "sites"])
 product_fieldsets = list(product_fieldsets)
 product_fieldsets.append((_("Other products"), {
     "classes": ("collapse-closed",),
